536a9fe534a46c1765a99d747de966bf091b7de3
Mjmarek <monicamarek24@gmail.com>
2018-01-18 15:15:36 -0500

    time's up

--- Pacman-Challenge/Program.cs
+++ Pacman-Challenge/Program.cs
@@ -6,10 +6,60 @@ using System.Threading.Tasks;
 
 namespace Pacman_Challenge
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private string input = @"Dot,Dot,Dot,Dot,Dot,Dot,Dot,Dot,Dot,Dot,Dot";
+
+        //needs static void Main(string[] args) since this is a console app
+
+        public Program(string input)
         {
+            this.input = input;
         }
+        
+        public int SolvePoints()
+        {
+            var points = 5000;
+
+            var replaceInput = input.Replace("Dot","10");
+
+            var inputClean = replaceInput.Split(',');
+
+            foreach (var moves in inputClean)
+            {
+                var movePoint = moves.Select(move => Convert.ToInt32(move)).ToArray();
+
+                var sum = movePoint.Sum();
+
+                points += sum;
+            }
+
+            return points;
+
+        }
+
+        //public int SolveLives()
+        //{
+            //var livesGained = 0;
+
+            //return livesGained;
+
+        //}
+        
     }
+    //Console.WriteLine($"Total points: {points}, Lives gained: {livesGained}");
 }
+
+
+
+//TODO
+//read input
+//parse input (remove commmas)
+//convert to array
+//assign moves to points
+//interate over array
+
+//track points(start with 5000)
+//track gained lives (start with 0)
+//print to console
+
---
+++ UnitTestProject1/PacmanTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Pacman_Challenge;
+
+namespace Pacman_Challenge.test
+{
+    [TestClass]
+    public class PacmanTest
+    {
+        [TestMethod]
+        public void ShouldReturn5110ForExample()
+        {
+            var example = @"Dot,Dot,Dot,Dot,Dot,Dot,Dot,Dot,Dot,Dot,Dot";
+
+            var result = new Program(example).SolvePoints();
+
+            Assert.AreEqual(5110, result);
+        }
+    }
+}
